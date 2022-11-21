eed61df7e0fb0dd877b4b6f58a92bfacdc1f2464
faridtb <faridtb@code.edu.az>
2022-11-21 23:18:43 +0400

    feat:MVC base app created

---
+++ TeamApp/Data/AppDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeamApp.Models;
+
+namespace TeamApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().HasData(
+             new User
+             {
+                 Id = 1,
+                 Username = "admin",
+                 Password = "123"
+             },
+              new User
+              {
+                  Id = 2,
+                  Username = "member",
+                  Password = "123"
+              },
+              new User
+              {
+                  Id = 3,
+                  Username = "client",
+                  Password = "123"
+              }
+             );
+        }
+
+    }
+}
