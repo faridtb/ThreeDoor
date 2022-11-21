a06bec863b48e07d961cef621f89501bbbe500d5
muradav <muradav@code.edu.az>
2022-11-21 23:48:19 +0400

    feat: test controller created

---
+++ TeamApp/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeamApp.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult<string> GetTest()
+        {
+            return "test text";
+        }
+    }
+}
